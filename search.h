9eb926e75c79148cbf1ce85664b523fc31e3a39d
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2019-06-09 19:28:05 +0800

    Create search.h

---
+++ search.h
@@ -0,0 +1,7 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H
+// Поиск треугольников максимальной площади
+void searchLargestTriangles(const Point* pointArray, int pointNum, Triangle* trArray, int maxTrNum);
+
+#endif
+
